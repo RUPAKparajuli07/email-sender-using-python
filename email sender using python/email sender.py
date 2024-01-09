ade58976b55473af8bd8aa47089a4e0884cfcbb5
not ∞ <110843279+RUPAKparajuli07@users.noreply.github.com>
2024-01-09 19:56:21 +0545

    Update email sender.py

--- email sender using python/email sender.py
+++ email sender using python/email sender.py
@@ -2,7 +2,7 @@ from email.message import EmailMessage
 import ssl  
 import smtplib
 import tkinter as tk
-from tkinter import messagebox
+from tkinter import messagebox 
  
 def send_email():
     email_sender = from_entry.get()
